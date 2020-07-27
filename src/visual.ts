e1ca8041cf63f53a060e255b8db1367b19f00ce4
daniil <dtkachenko41@gmail.com>
2020-07-27 20:13:54 +0300

    add d3 and simple properties

--- src/visual.ts
+++ src/visual.ts
@@ -22,56 +22,13 @@ export class Visual implements IVisual {
     private monthText: d3.Selection<HTMLHeadingElement, unknown, null, undefined>;
     private yearText: d3.Selection<HTMLHeadingElement, unknown, null, undefined>;
     private weekdays = ["su", "mo", "tu", "we", "th", "fr", "sa"];
-    //private weekdays = ["sos", "mos", "tus", "wes", "ths", "frs", "sas"];
-    private theadRow: d3.Selection<HTMLTableRowElement, unknown, HTMLElement, any>;
-    private arrayDate:Date[] = new Array();
+    private highlightWeekdays: boolean = true;
+
+    private tbody: d3.Selection<HTMLTableSectionElement, unknown, null, undefined>
+    private arrayDate: Date[] = new Array();
 
     constructor(options: VisualConstructorOptions) {
         this.target = options.element;
-        if (document) {
-
-            //debugger;
-
-            try {
-
-                let table = d3.select(this.target)
-                    .append("table")
-                    .classed("table", true)
-                    .attr("id", "calendar");
-
-                let theadTbody = table
-                    .append("thead");
-
-                let tbody = d3.select("table")
-                    .append("tbody");
-
-                this.theadRow = theadTbody
-                    .append("tr");
-
-                this.theadRow
-                    .selectAll("th")
-                    .data(this.weekdays)
-                    .enter()
-                    .append("th")
-                    .text(d => d);
-
-                tbody
-                    .append("tr")
-                    .selectAll("td")
-                    .data(this.weekdays)
-                    .enter()
-                    .append("td")
-                    .text(d => d);
-
-
-            }
-            catch (e) {
-                debugger;
-            }
-
-        //Лучше не вставлять html строки хардкодом, а использовать методы d3 для этого.
-        /*let row = "<table class='table' id='calendar' border='1'><thead><tr><th>su</th><th>mo</th><th>tu</th><th>we</th><th>th</th><th>fr</th><th>sa</th></tr></thead><tbody></tbody></table>";
-        this.target.innerHTML += row;
 
         this.monthText = d3.select(this.target)
             .append("h3")
@@ -79,15 +36,19 @@ export class Visual implements IVisual {
         this.yearText = d3.select(this.target)
             .append("h3")
             .text("year");
+
         const buttonLeft = d3.select(this.target)
             .append("input")
             .attr("class", "switch")
             .attr("value", "<")
             .attr("type", "button")
             .on("click", () => {
-                this.date = new Date(this.date.getFullYear(), this.date.getMonth()-1, 1);
-                this.target.getElementsByTagName('tbody')[0].innerHTML = '';
-                this.fillCalendar();});
+                this.date = new Date(this.date.getFullYear(), this.date.getMonth() - 1, 1);
+                this.tbody
+                    .selectAll("tr")
+                    .remove();
+                this.fillCalendar();
+            });
 
         const buttonRight = d3.select(this.target)
             .append("input")
@@ -95,23 +56,49 @@ export class Visual implements IVisual {
             .attr("value", ">")
             .attr("type", "button")
             .on("click", () => {
-                this.date = new Date(this.date.getFullYear(), this.date.getMonth()+1, 1);
-                this.target.getElementsByTagName('tbody')[0].innerHTML = '';
-                this.fillCalendar();});
+                this.date = new Date(this.date.getFullYear(), this.date.getMonth() + 1, 1);
+                this.tbody
+                    .selectAll("tr")
+                    .remove();
+                this.fillCalendar();
+            });
+
+        if (document) {
+            try {
+
+                let table = d3.select(this.target)
+                    .append("table")
+                    .classed("table", true);
+
+                let thead = table
+                    .append("thead");
+
+                this.tbody = table.append("tbody");
+
+                thead
+                    .selectAll("th")
+                    .data(this.weekdays)
+                    .enter()
+                    .append("th")
+                    .text(d => d);
+            }
+            catch (e) {
+                debugger;
+            }
 
             this.date = new Date();
             this.date.setDate(1);
 
             this.fillCalendar();
-
-            */
         }
     }
 
     public update(options: VisualUpdateOptions) {
         this.settings = Visual.parseSettings(options && options.dataViews && options.dataViews[0]);
 
-        let arrayDate:Date[] = new Array();
+        this.updateSettings(options);
+
+        let arrayDate: Date[] = new Array();
         this.arrayDate = this.getAllweekends(options);
 
         this.fillCalendar();
@@ -126,77 +113,111 @@ export class Visual implements IVisual {
             || !dv[0].categorical.categories[0].values)
             return this.arrayDate;
 
-            debugger;
+        debugger;
 
         let view = dv[0].categorical;
         let categories = view.categories[0];
         let values = categories.values;
-
-        //лучше сделать так
-        //for (let i = 0; i < values.length; i++) { 
+ 
         for (let i = 0, len = values.length; i < len; i++) {
-            let currentDate: Date = <Date>values[i];//new Date(values[i].toString());
+            let currentDate: Date = <Date>values[i];
             this.arrayDate.push(currentDate);
         }
 
         return this.arrayDate;
     }
 
-    private getAllDates(dayOfWeek: number, prevMonthLastDay: Date, lastDay: number): number[][] { // заполнение массива дней месяца. только для d3
-        let allDates: number[][];
-        return allDates;
-    }
-
     private fillCalendar() {
-        this.target.getElementsByTagName('tbody')[0].innerHTML = '';
+
         this.newDate = new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate());
         let month = this.date.toLocaleString('ru', {
-          month: 'long'
+            month: 'long'
         });
         this.monthText
             .text(month);
         this.yearText
             .text(this.date.getFullYear());
-        
+
         var prevMonthLastDay = new Date(this.newDate.getFullYear(), this.newDate.getMonth(), 0);
         let lastDay = new Date(this.newDate.getFullYear(), this.newDate.getMonth() + 1, 0).getDate();
         let dayOfWeek = this.newDate.getDay();
 
         this.newDate.setDate(this.newDate.getDate() - dayOfWeek);
 
-        let row = '';
-        row += '<tr>';
+        this.tbody
+            .selectAll("tr")
+            .remove();
+        let currentRow = this.tbody
+            .append("tr");
+
         if (dayOfWeek != 0) {
-          for(let i = this.newDate.getDate(); i != prevMonthLastDay.getDate()+1; i++){
-            row += '<td></td>'
-            this.newDate.setDate(this.newDate.getDate() + 1);
-          }
+            for (let i = this.newDate.getDate(); i != prevMonthLastDay.getDate() + 1; i++) {
+                currentRow
+                    .append("td");
+                this.newDate.setDate(this.newDate.getDate() + 1);
+            }
         }
-        for(let j = this.newDate.getDate(); j != lastDay + 1; j ++){
-          if(this.newDate.getDay() == 0){
-            row += '</tr><tr>'
-          }
-          if (this.newDate.getDay() == 0 || this.newDate.getDay() == 6 || this.arrayDate.some(date => {
-            return date.getFullYear() == this.newDate.getFullYear() && date.getMonth() == this.newDate.getMonth() && date.getDate() == this.newDate.getDate();
-          }))
-          {
-            row += "<td>" + '<font color="red">' + this.newDate.getDate() + "</font>" + "</td>";
-            this.newDate.setDate(this.newDate.getDate() + 1);
-          }
-          else {
-            row += "<td>" + this.newDate.getDate() + "</td>";
-            this.newDate.setDate(this.newDate.getDate() + 1);
-          }
+        for (let j = this.newDate.getDate(); j != lastDay + 1; j++) {
+            if (this.newDate.getDay() == 0) {
+                currentRow = this.tbody
+                    .append("tr");
+            }
+            if (this.newDate.getDay() == 0 || this.newDate.getDay() == 6 || this.arrayDate.some(date => {
+                return date.getFullYear() == this.newDate.getFullYear() && date.getMonth() == this.newDate.getMonth() && date.getDate() == this.newDate.getDate();
+            })) {
+                currentRow
+                    .append("td")
+                    //.classed("weekdays", true)
+                    .attr("style", this.highlightWeekdays ? "color:red" : "")
+                    .text(this.newDate.getDate());
+                this.newDate.setDate(this.newDate.getDate() + 1);
+            }
+            else {
+                currentRow
+                    .append("td")
+                    .text(this.newDate.getDate());
+                this.newDate.setDate(this.newDate.getDate() + 1);
+            }
         }
-        row += '</tr>'
-        this.target.getElementsByTagName('tbody')[0].innerHTML += row;
     }
 
     private static parseSettings(dataView: DataView): VisualSettings {
         return <VisualSettings>VisualSettings.parse(dataView);
     }
 
+    private updateSettings(option: VisualUpdateOptions) {
+        let dv = option.dataViews;
+
+        debugger;
+        if( !dv
+            || !dv[0]
+            || !dv[0].metadata
+            || !dv[0].metadata.objects
+            || !dv[0].metadata.objects.highlightWeekdays)
+            return null;
+        
+        let metadata = dv[0].metadata;
+        let objects = metadata.objects;
+        let highlightWeekdays = objects.highlightWeekdays;
+        this.highlightWeekdays = <boolean>highlightWeekdays.show;
+        console.log(highlightWeekdays.show);
+    }
+
     public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstance[] | VisualObjectInstanceEnumerationObject {
-        return VisualSettings.enumerateObjectInstances(this.settings || VisualSettings.getDefault(), options);
+        let propertyGroupName = options.objectName;
+        let properties: VisualObjectInstance[] = [];
+
+        switch (propertyGroupName) {
+            case "highlightWeekdays":
+                properties.push({
+                    objectName: propertyGroupName,
+                    properties: {
+                        show: this.highlightWeekdays
+                    },
+                    selector: null
+                });
+                break;
+        };
+        return properties;
     }
 }
